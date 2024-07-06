3a7c78be0a36a482d755318c528170b6adeefa9a
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2024-07-06 12:55:02 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -3,13 +3,13 @@ from sample_config import Config
 
 class Development(Config):
     # ضـع اكـوادك تحت مكـان الكـلام العـربي  .. امسـح الكلام العربي اللي بين " " فقـط وضع اكـوادك
-    APP_ID = 6 #ضع كود الايبي ايدي الصغيـر مكان الرقم 6 نسخ لصق   
-    API_HASH = "ضع كود الايبي هاش"
-    ALIVE_NAME = "اسم حسابك التلي"
-    DB_URI = "رابـط التخـزين الخـاص بك"
-    STRING_SESSION = "كود تيرمــكس"
-    TG_BOT_TOKEN = "توكـن البـوت الخـاص بك"
-    PRIVATE_GROUP_BOT_API_ID = -100  # ضـع ايـدي كـروب السجـل بجـانب العـدد -100 لا تمسـح العـدد هـذا
+    APP_ID = 20559750 #ضع كود الايبي ايدي الصغيـر مكان الرقم 6 نسخ لصق   
+    API_HASH = "17e71611711a2b56ef360b2e3f5fcc95"
+    ALIVE_NAME = "محمد"
+    DB_URI = "postgres://koyeb-adm:IROvN1pwA3dc@ep-little-queen-a4gpgb67.us-east-1.pg.koyeb.app/koyebdb"
+    STRING_SESSION = "BABVDzLewbeedk_oPWi44GzmMTHm2KVYVpPmtQk8Fw_uASID8W1k3oWNbZV9kZppGYYtdQFoSWsjuLc6N8GEBm1dGF8vMMfy_-y-wPyX_ruNctnihz9lFozXToyS9PL-m2LAU8kfO_5B0Pgkr3vRagF2ZJtub4KNrvUXUa3Eh9eaBvj7zsf5yOMomTll8OiwIo5v4hmqCUX1igTyLxiSfDQPP2DDAuChoQsVs30dQOlpz-nrLaReozZmdyHclarLFa8R4B9xs-_TpnuZTdjEKqU-U7_PmQna6EYT4I_YHKKqsKiimzrin2PdjNLLc7ZPGw4IpQ-l9aIWKc_DQN2oN54HAAAAAb2gEekA"
+    TG_BOT_TOKEN = "6706505656:AAEYBm87YNIUEfCskNZpVKKvGmHltahryts"
+    PRIVATE_GROUP_BOT_API_ID = -1002178794935  # ضـع ايـدي كـروب السجـل بجـانب العـدد -100 لا تمسـح العـدد هـذا
     COMMAND_HAND_LER = "."  # اتركهــا كمـا هـي
     SUDO_COMMAND_HAND_LER = "."  # اتركهــا كمـا هـي
     TZ = "Asia/Baghdad"  # اتركهــا كمـا هـي
